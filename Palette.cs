c6e9d64f296e387b1d38dbab1b945a678d6f15e1
Sergeiphantom86 <96051914+Sergeiphantom86@users.noreply.github.com>
2024-05-21 02:29:49 +0300

    Color.cs

--- Palette.cs
+++ Palette.cs
@@ -1,28 +1,25 @@
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
-public class Palette : MonoBehaviour
+public class Color : MonoBehaviour
 {
-    private List<Color> _colors = new();
+    private readonly List<UnityEngine.Color> _colors = new();
 
-    public void GetTexturs()
+    public void Replace()
     {
-        Add();
+        AddColors();
 
-        gameObject.GetComponent<Renderer>().material.color = _colors.ElementAt(Separat.Random(0, _colors.Count));
+        gameObject.GetComponent<Renderer>().material.color = _colors[Random.Range(0, _colors.Count)];
     }
 
-    private void Add()
+    private void AddColors()
     {
-        _colors.Add(Color.red);
-        _colors.Add(Color.green);
-        _colors.Add(Color.blue);
-        _colors.Add(Color.magenta);
-        _colors.Add(Color.yellow);
-        _colors.Add(Color.white);
-        _colors.Add(Color.cyan);
-        _colors.Add(Color.black);
-        _colors.Add(Color.gray);
+        _colors.Add(UnityEngine.Color.red);
+        _colors.Add(UnityEngine.Color.green);
+        _colors.Add(UnityEngine.Color.blue);
+        _colors.Add(UnityEngine.Color.magenta);
+        _colors.Add(UnityEngine.Color.yellow);
+        _colors.Add(UnityEngine.Color.white);
+        _colors.Add(UnityEngine.Color.cyan);
     }
 }
